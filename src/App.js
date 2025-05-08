22f52b4c52b4be9526cd4dfdb675871f0c841e3a
hyhchan8 <hyhchan8@gmail.com>
2025-05-08 21:21:59 +0900

    todo과제 업로드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [toDo,setToDo] = useState("");
+  const[toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  
+  const handleDelete = (deleteIndex) => {
+    setToDos(currentArray => 
+      currentArray.filter((_, index)=>index !== deleteIndex)
+
+    );
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="Write your to do...">
+
+        </input>
+        <button className="addButton">Add To Do</button>
+      </form>
+      <hr />
+      <ul className="todoList">
+        {toDos.map((item,index) =>( 
+          <li key={index} className="todoItem">
+            {item}
+            <button className="Dij"
+            onClick={()=> handleDelete(index)}>
+              ❌
+            </button>
+            </li>
+          ))}
+      </ul>
     </div>
+     
   );
 }
 
