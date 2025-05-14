9925d9191d7f32dc26414be4bb07b6ee71e80ee2
hyhchan8 <hyhchan8@gmail.com>
2025-05-14 21:06:17 +0900

    main 수정

---
+++ todo_-/src/App.js
@@ -0,0 +1,48 @@
+
+import './App.css';
+import { useState } from "react";
+
+
+function App() {
+  const [toDo,setToDo] = useState("");
+  const[toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  
+  return (
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="Write your to do...">
+
+        </input>
+        <button className="addButton">Add To Do</button>
+      </form>
+      <hr />
+      <ul className="todoList">
+        {toDos.map((item,index) =>( 
+          <li key={index} className="todoItem">
+            {item}
+            
+            </li>
+          ))}
+      </ul>
+    </div>
+     
+  );
+}
+
+export default App;
